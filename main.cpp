ebcf4fcf3bd679976cd27f7116cc15b24d9134ad
Gavin Tranquilino <49102766+gavintranquilino@users.noreply.github.com>
2024-01-02 17:17:09 +0100

    Basic structure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <fstream>
 
+char pixelToChar(int pixelVal);
+
+void image2Ascii(const std::ifstream &input, std::ostream &output); 
+
 int main()
 {
-  std::cout << "Initial Commit :)";
-  return 0;
+  std::ifstream fin("input.jpg");
+	
+	if (!fin.is_open())
+	{
+	    std::cout << "Unable to open file";
+	    fin.close();
+	    return 1;
+	}
+	
+	return 0;	
 }
